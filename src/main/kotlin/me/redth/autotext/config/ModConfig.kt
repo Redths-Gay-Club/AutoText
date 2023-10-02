3c0d0cceedd78ed9276ec9db09fa111df2ab0482
Redth <redth1206@proton.me>
2023-10-02 23:56:44 +0800

    readability

--- src/main/kotlin/me/redth/autotext/config/ModConfig.kt
+++ src/main/kotlin/me/redth/autotext/config/ModConfig.kt
@@ -14,17 +14,29 @@ object ModConfig : Config(Mod(AutoText.NAME, ModType.UTIL_QOL), "${AutoText.MODI
     @CustomOption
     private var entries = emptyArray<KeyTextEntry>()
 
-    override fun getCustomOption(field: Field, annotation: CustomOption, page: OptionPage, mod: Mod, migrate: Boolean) = OptionList.also {
+    override fun getCustomOption(
+        field: Field,
+        annotation: CustomOption,
+        page: OptionPage,
+        mod: Mod,
+        migrate: Boolean
+    ) = OptionList.also {
         ConfigUtils.getSubCategory(page, "General", "").options.add(it)
     }
 
     override fun load() {
         super.load()
-        OptionList.list = entries.map { KeyTextEntryOption(it) }.toMutableList()
+
+        OptionList.list = entries.map {
+            KeyTextEntryOption(it)
+        }.toMutableList()
     }
 
     override fun save() {
-        entries = OptionList.list.map { it.keyTextEntry }.toTypedArray()
+        entries = OptionList.list.map {
+            it.keyTextEntry
+        }.toTypedArray()
+
         super.save()
     }
 }
